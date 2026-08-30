adb1e08b4e6f5b7c8e806b71bb9829f779a715de
sdsdss <yj65581690@gmail.com>
2026-08-31 03:59:03 +0900

    supabase.js 업데이트

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://dtwtvjrqrweclbkrckeh.supabase.co";
-const SUPABASE_KEY = "여기에 네 publishable key";
+const SUPABASE_KEY = "sb_publishable_th-F0YuEsB6JNtOjT8s-bw_gSPocZdU";
 
 const db = window.supabase.createClient(
     SUPABASE_URL,
