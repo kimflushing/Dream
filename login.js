e8d2bcc82f934b8c3db924caf360c0588743b1dd
sdsdss <yj65581690@gmail.com>
2026-08-31 01:02:18 +0900

    login.js 업데이트

--- login.js
+++ login.js
@@ -1,29 +1,24 @@
 const loginForm = document.getElementById("loginForm");
 
-loginForm?.addEventListener("submit", async (e) => {
+loginForm.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
-    try {
-        alert("로그인 시작");
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
 
-        const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value;
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    });
 
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email,
-            password
-        });
-
-        if (error) {
-            alert("에러: " + error.message);
-            return;
-        }
-
-        alert("로그인 성공!");
-
-        location.href = "index.html";
-
-    } catch (err) {
-        alert("오류: " + err.message);
+    if (error) {
+        alert("로그인 실패\n\n" + error.message);
+        return;
     }
+
+    alert("로그인 성공!");
+
+    location.href = "index.html";
+
 });
