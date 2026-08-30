2ba513c087ae46c8c28a0a0f89e7ecf541c743b1
sdsdss <yj65581690@gmail.com>
2026-08-31 01:15:25 +0900

    supabase.js 업데이트

--- supabase.js
+++ supabase.js
@@ -1,8 +1,12 @@
+// =========================
+// Supabase 연결
+// =========================
+
 const supabaseUrl = "https://dtwtvjrqrweclbkrckeh.supabase.co";
 
-const supabaseAnonKey = "sb_publishable_th-F0YuEsB6JNtOjT8s-bw_gSPocZdU";
+const supabaseKey = "sb_publishable_th-F0YuEsB6JNtOjT8s-bw_gSPocZdU";
 
 const supabase = window.supabase.createClient(
     supabaseUrl,
-    supabaseAnonKey
+    supabaseKey
 );
